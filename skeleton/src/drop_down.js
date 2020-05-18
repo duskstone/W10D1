a670cb40d8a09686c205bf6e1d0ba39f811942a6
duskstone <jedgardromero@gmail.com>
2020-05-18 14:19:14 -0400

    added Li constructor function and appended to Ul

--- skeleton/src/drop_down.js
+++ skeleton/src/drop_down.js
@@ -1,5 +1,6 @@
 
-const dogs = {
+
+ const dogs = {
   "Corgi": "https://www.akc.org/dog-breeds/cardigan-welsh-corgi/",
   "Australian Shepherd": "https://www.akc.org/dog-breeds/australian-shepherd/",
   "Affenpinscher": "https://www.akc.org/dog-breeds/affenpinscher/",
@@ -9,3 +10,29 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+export function dogLinkCreator(){
+  let taggedDogs = [];
+
+  for(let name in dogs) {
+    let link = dogs[name];
+
+    let a = document.createElement('A');
+    a.innerHTML = name;
+    a.setAttribute('href', link);
+
+    let li = document.createElement('LI');
+    li.classList.add('dog-link');
+    li.appendChild(a);
+
+    taggedDogs.push(li);
+  }
+  return taggedDogs;
+}
+
+export function attachDogLinks(){
+  let dogLinks = dogLinkCreator();
+  let ul = document.querySelector('.drop-down-dog-list');
+  dogLinks.forEach(dog => ul.append(dog));
+}
+
+attachDogLinks();
