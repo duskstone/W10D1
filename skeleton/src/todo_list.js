753c713df814ea29d1848eded8aed442ed2daa12
duskstone <jedgardromero@gmail.com>
2020-05-18 16:48:48 -0400

    added functionality allowing list items to appear on sumbition

--- skeleton/src/todo_list.js
+++ skeleton/src/todo_list.js
@@ -1,2 +1,31 @@
+let todos = [];
 
+const ul = document.querySelector('.todos');
+const form = document.querySelector('.add-todo-form');
 
+function addTodo(e){
+    e.preventDefault();
+    // debugger
+    let input = document.querySelector('[name=add-todo]');
+    let value = input.value;
+    let toDo = {done: false, value};
+    todos.push(toDo);
+    populateList(todos) 
+    localStorage.setItem('todos', JSON.stringify(todos));
+    e.currentTarget.reset();
+}
+
+function populateList(todos) {
+    todos.forEach(toDo => {
+        // debugger
+        const input = document.createElement('input');
+        input.setAttribute('type','checkbox');
+        const li = document.createElement('li');
+        // debugger
+        li.innerHTML = input.outerHTML + toDo.value;
+        ul.append(li);
+        todos.shift();
+    })
+}
+
+form.addEventListener("submit", addTodo);
